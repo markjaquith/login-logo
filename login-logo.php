03bc4bc6f880f70a790c47548e91b26be7978bbe
Mark Jaquith <m@rkj.me>
2012-04-24 06:16:11 +0000

    Update Trunk version to 0.6. Add licensing stuff. Add link to plugin page.

--- login-logo.php
+++ login-logo.php
@@ -1,11 +1,30 @@
 <?php
 /*
 Plugin Name: Login Logo
-Description: Drop a PNG file named <code>login-logo.png</code> into your <code>wp-content</code> directory. This simple plugin takes care of the rest, with zero configuration. Transparent backgrounds work best. Keep the width below 326 pixels.
-Version: 0.6-beta-1
+Description: Drop a PNG file named <code>login-logo.png</code> into your <code>wp-content</code> directory. This simple plugin takes care of the rest, with zero configuration. Transparent backgrounds work best. Crop it tight, with a width of 312 pixels, for best results.
+Version: 0.6
 License: GPL
+Plugin URI: http://txfx.net/wordpress-plugins/login-logo/
 Author: Mark Jaquith
 Author URI: http://coveredwebservices.com/
+
+==========================================================================
+
+Copyright 2011-2012  Mark Jaquith
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 class CWS_Login_Logo_Plugin {
